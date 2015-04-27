9536a1c76dadd73cf6df82af489b3e817394dbca
josedavid <josedavid@debianico.umons.ac.be>
2015-04-27 18:34:52 +0200

    clearbuild.py equivalent to make clean, no need to do it every single time we compile

---
+++ clearbuild.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2015 Jose David Fernandez Rodriguez
+#  
+# This file is distributed under the terms of the
+# GNU Affero General Public License, version 3
+# as published by the Free Software Foundation.
+# 
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public
+# License along with this file. You may obtain a copy of the License at
+# http://www.gnu.org/licenses/agpl-3.0.txt
+
+import os
+import shutil
+
+pynames = ["_pyslic3r"]
+
+# clean previous build
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+      for pyname in pynames:
+        if (name.startswith(pyname) and not(name.endswith(".pyx") or name.endswith(".pxd"))):
+            os.remove(os.path.join(root, name))
+    for name in dirs:
+        if (name == "build"):
+            shutil.rmtree(name)
