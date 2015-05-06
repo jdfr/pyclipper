de07e9e4d5b52ce625e799abffa2d14ab66abbe7
josedavid <josedavid@debianico.umons.ac.be>
2015-05-06 16:54:21 +0200

    after reworking the build system, there is no need to do any hack to link the shared libraries

--- pyslic3r/__init__.py
+++ pyslic3r/__init__.py
@@ -13,21 +13,5 @@
 # License along with this file. You may obtain a copy of the License at
 # http://www.gnu.org/licenses/agpl-3.0.txt
 
-#from ._pyslic3r import *
-
-#VERY UGLY HACK TO GET LD TO LINK TO './libslic3rlib.so' WITHOUT INSTALLING IT 
-def _dynamicLoadHack():
-  from ctypes import cdll
-  import os.path
-  cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libslic3rlib.so'))
-
-try:
-  from ._SlicedModel import *
-except ImportError:
-  #probably the linker cannot find libslicerlib.so because it is not installed.
-  #Trying a hack...
-  _dynamicLoadHack()
-  #trying again...
-  from ._SlicedModel import *
-
+from ._SlicedModel  import *
 from ._TriangleMesh import *
