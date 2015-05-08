b77a4698dca687c014aad5ce82232396066c15d7
josedavid <josedavid@debianico.umons.ac.be>
2015-05-08 17:19:45 +0200

    refactor interface of _Clipper extension types and fix bugs

--- pyslic3r/_Clipper.pxd
+++ pyslic3r/_Clipper.pxd
@@ -3,45 +3,25 @@ from libcpp cimport bool
 cimport numpy as cnp
 
 cdef class ClipperPaths:
-  cdef c.Paths *thisptr
-
-  cpdef              reverse        (self)
+  cdef  c.Paths   * thisptr
   cdef  c.Paths   * _simplify       (self, c.Paths *out, c.PolyFillType fillType=*) nogil
-  cpdef ClipperPaths simplify       (self, int fillType=*)
-  cpdef              simplifyInPlace(self, int fillType=*)
   cdef  c.Paths   * _clean          (self, c.Paths *out, double distance=*) nogil
-  cpdef ClipperPaths clean          (self, double distance=*)
-  cpdef              cleanInPlace   (self, double distance=*)
-  cpdef unsigned int numPaths       (self)
-  cpdef bool         orientation    (self, unsigned int npath)
-  cpdef double       area           (self, unsigned int npath)
-  cpdef int          pointInPolygon (self, unsigned int npath, int x, int y)
-  cpdef cnp.ndarray                        orientations(self)
-  cpdef cnp.ndarray[cnp.float64_t, ndim=1] areas       (self)
 
 cdef class ClipperPolyTree:
-  cdef c.PolyTree *thisptr
-  cdef c.Paths *toPaths(self, c.Paths *output=*)
-  cpdef ClipperPaths toClipperPaths      (self)
-  cpdef ClipperPaths toClipperPathsByType(self, bool closed)
+  cdef c.PolyTree *  thisptr
+  cdef c.Paths    *  toPaths(self, c.Paths *output=*)
 
 cdef class ClipperClip:
-  cdef c.Clipper     *thisptr
-  cdef c.PolyFillType subjectfill
-  cdef c.PolyFillType clipfill
-  cdef c.ClipType     cliptype
-  cpdef bool AddSubjects(self, ClipperPaths paths, bool pathsAreClosed=*)
-  cpdef bool AddClips   (self, ClipperPaths paths)
-  cpdef Clear(self)
-  cpdef bool Execute            (self, object          solution=*)
-  cpdef bool ExecuteWithPaths   (self, ClipperPaths    solution=*)
-  cpdef bool ExecuteWithPolyTree(self, ClipperPolyTree solution=*)
+  cdef  c.Clipper     *thisptr
+  cdef  c.PolyFillType subjectfill
+  cdef  c.PolyFillType clipfill
+  cdef  c.ClipType     cliptype
+  cpdef bool AddPaths           (self, ClipperPaths    paths, c.PolyType typ, bool pathsAreClosed=*)
+  cpdef bool ExecuteWithPaths   (self, ClipperPaths    solution)
+  cpdef bool ExecuteWithPolyTree(self, ClipperPolyTree solution)
 
 cdef class ClipperOffset:
-  cdef c.ClipperOffset *thisptr
-  cdef double delta
-  cpdef AddPaths(self, ClipperPaths paths, int joinType=*, int endType=*)
-  cpdef Clear(self)
-  cpdef Execute            (self, object          solution=*)
-  cpdef ExecuteWithPaths   (self, ClipperPaths    solution=*)
-  cpdef ExecuteWithPolyTree(self, ClipperPolyTree solution=*)
+  cdef  c.ClipperOffset *thisptr
+  cdef  double _delta
+  cpdef ExecuteWithPaths   (self, ClipperPaths    solution)
+  cpdef ExecuteWithPolyTree(self, ClipperPolyTree solution)
--- pyslic3r/_Clipper.pyx
+++ pyslic3r/_Clipper.pyx
@@ -17,8 +17,9 @@ cnp.import_array()
 cdef extern from "numpy/ndarraytypes.h" nogil:
   int NPY_ARRAY_CARRAY
 
-cdef extern from "math.h" nogil:
-  double fabs(double)
+cimport cpython.ref as ref
+
+
 
 #enum ClipType
 ctIntersection      = c.ctIntersection
@@ -48,7 +49,44 @@ etOpenButt          = c.etOpenButt
 etOpenSquare        = c.etOpenSquare
 etOpenRound         = c.etOpenRound
 
+cdef class ClipperPathsIterator:
+  cdef ClipperPaths paths
+  cdef unsigned int current
+  
+  def __cinit__(self, ClipperPaths p):
+    self.paths   = p
+    self.current = 0
+    
+  @cython.boundscheck(False)
+  def __next__(self):
+    if self.current >= self.paths.thisptr[0].size():
+      raise StopIteration
+    else:
+      x = Path2arrayView(self.paths, &self.paths.thisptr[0][self.current])
+      self.current += 1
+      return x
 
+
+@cython.boundscheck(False)
+def arrayListToClipperPaths(list paths, ClipperPaths output=None):
+  """Convert a list of two-dimensional arrays of type int64 to a ClipperPaths 
+  object (this list can be got with list(x for x in paths)"""
+  cdef c.Path *path
+  cdef unsigned int k, p, npaths, npoints
+  cdef cnp.ndarray[cnp.int64_t, ndim=1] array
+  if output is None:
+    output = ClipperPaths()
+  npaths = len(paths)
+  output.thisptr[0].resize(npaths)
+  for k in range(npaths):
+    array   = paths[k]
+    npoints = array.shape[0]
+    path    = &output.thisptr[0][k]
+    path[0].resize(npoints)
+    for p in range(npoints):
+      path[0][p].X = array[p,0]
+      path[0][p].Y = array[p,1]
+    
 
 cdef class ClipperPaths:
   """Thin wrapper around Clipper::Paths. It is intended just as a temporary object
@@ -57,8 +95,35 @@ cdef class ClipperPaths:
 
   def __cinit__(self):       self.thisptr = new c.Paths()
   def __dealloc__(self): del self.thisptr
+
+  def __reduce__(self):
+    d = {'state': list(self.__iter__())}
+    return (ClipperPaths, (), d)
+  def __setstate__(self, d):
+    arrayListToClipperPaths(d['state'], self)
+    
+  def __len__(self):
+    return self.thisptr[0].size()
+  def __iter__(self):
+    return ClipperPathsIterator(self)
   
-  cpdef reverse(self):
+  def __getitem__(self, val):
+    """Basic indexing support"""
+    cdef int npath
+    if   isinstance(val, int):
+      npath = val
+      if (npath<0) or (<unsigned int>npath>=self.thisptr[0].size()):
+        raise Exception('Invalid index')
+      return Path2arrayView(self.paths, &self.thisptr[0][npath])
+    elif isinstance(val, slice) or isinstance(val, tuple):
+      raise Exception('This object does not support slicing, only indexing')
+    else:
+      raise IndexError('Invalid slice object')
+  
+  def clear(self):
+    self.thisptr[0].clear()
+  
+  def reverse(self):
     c.ReversePaths(self.thisptr[0])
   
   cdef c.Paths * _simplify(self, c.Paths *out, c.PolyFillType fillType=c.pftEvenOdd) nogil:
@@ -67,12 +132,12 @@ cdef class ClipperPaths:
     c.SimplifyPolygons(self.thisptr[0], out[0], fillType)
     return out
   
-  cpdef ClipperPaths simplify(self, int fillType=c.pftEvenOdd):
+  def simplify(self, int fillType=c.pftEvenOdd):
     cdef ClipperPaths out = ClipperPaths()
     out.thisptr = self._simplify(out.thisptr, <c.PolyFillType>fillType)
     return out
 
-  cpdef simplifyInPlace(self, int fillType=c.pftEvenOdd):
+  def simplifyInPlace(self, int fillType=c.pftEvenOdd):
     c.SimplifyPolygons(self.thisptr[0], <c.PolyFillType>fillType)
   
   cdef c.Paths * _clean(self, c.Paths *out, double distance=1.415) nogil:
@@ -81,22 +146,22 @@ cdef class ClipperPaths:
     c.CleanPolygons(self.thisptr[0], out[0], distance)
     return out
   
-  cpdef ClipperPaths clean(self, double distance=1.415):
+  def clean(self, double distance=1.415):
     cdef ClipperPaths out = ClipperPaths()
     out.thisptr = self._clean(out.thisptr, distance)
     return out
 
-  cpdef cleanInPlace(self, double distance=1.415):
+  def   cleanInPlace(self, double distance=1.415):
     c.CleanPolygons(self.thisptr[0], distance)
   
-  cpdef unsigned int numPaths(self):
-    return self.thisptr[0].size()
-  
-  cpdef bool orientation(self, unsigned int npath):
+  @cython.boundscheck(False)
+  def orientation(self, unsigned int npath):
+    if npath>=self.thisptr[0].size():
+      raise Exception('Invalid index')
     return c.Orientation(self.thisptr[0][npath])
     
   @cython.boundscheck(False)
-  cpdef cnp.ndarray orientations(self):
+  def orientations(self):
     cdef cnp.npy_intp length = self.thisptr[0].size()
     #using cnp.uint8_t is an ugly hack, but there is no cnp.bool_t
     cdef cnp.ndarray out = cnp.PyArray_EMPTY(1, &length, cnp.NPY_BOOL, 0)
@@ -105,11 +170,14 @@ cdef class ClipperPaths:
       out[k] = c.Orientation(self.thisptr[0][k])
     return out
   
-  cpdef double area(self, unsigned int npath):
+  @cython.boundscheck(False)
+  def area(self, unsigned int npath):
+    if npath>=self.thisptr[0].size():
+      raise Exception('Invalid index')
     return c.Area(self.thisptr[0][npath])
     
   @cython.boundscheck(False)
-  cpdef cnp.ndarray[cnp.float64_t, ndim=1] areas(self):
+  def areas(self):
     cdef cnp.npy_intp length = self.thisptr[0].size()
     cdef cnp.ndarray[cnp.float64_t, ndim=1] out = cnp.PyArray_EMPTY(1, &length, cnp.NPY_FLOAT64, 0)
     cdef unsigned int k
@@ -117,7 +185,10 @@ cdef class ClipperPaths:
       out[k] = c.Area(self.thisptr[0][k])
     return out
 
-  cpdef int pointInPolygon(self, unsigned int npath, int x, int y):
+  @cython.boundscheck(False)
+  def pointInPolygon(self, unsigned int npath, int x, int y):
+    if npath>=self.thisptr[0].size():
+      raise Exception('Invalid index')
     cdef c.IntPoint p
     p.X = x
     p.Y = y
@@ -131,6 +202,9 @@ cdef class ClipperPolyTree:
   def __cinit__(self):       self.thisptr = new c.PolyTree()
   def __dealloc__(self): del self.thisptr
 
+  def clear(self):
+    self.thisptr[0].Clear()
+    
   cdef c.Paths *toPaths(self, c.Paths *output=NULL):
     if output==NULL:
       output = new c.Paths()
@@ -139,12 +213,13 @@ cdef class ClipperPolyTree:
     c.PolyTreeToPaths(self.thisptr[0], output[0])
     return output
 
-  cpdef ClipperPaths toClipperPaths(self):
-    cdef ClipperPaths paths = ClipperPaths()
+  def toClipperPaths(self, ClipperPaths paths=None):
+    if paths is None:
+      paths                 = ClipperPaths()
     paths.thisptr           = self.toPaths(paths.thisptr)
     return paths
   
-  cpdef ClipperPaths toClipperPathsByType(self, bool closed):
+  def toClipperPathsByType(self, bool closed):
     cdef ClipperPaths paths = ClipperPaths()
     if closed:
       c.ClosedPathsFromPolyTree(self.thisptr[0], paths.thisptr[0])
@@ -181,36 +256,37 @@ cdef class ClipperClip:
     def __get__(self):   return self.cliptype
     def __set__(self, int val): self.cliptype    = <c.ClipType>val
 
-  def __cinit__  (self):
+  def __cinit__  (self, int clipType=c.ctIntersection, int clipFillType=c.pftEvenOdd, int subjectFillType=c.pftEvenOdd, bool reverseSolution=False, bool strictlySimple=False, bool preserveCollinear=False):
     self.thisptr = new c.Clipper()
-    self.subjectfill = c.pftEvenOdd
-    self.clipfill    = c.pftEvenOdd
-    self.cliptype    = c.ctIntersection
+    self.subjectfill = <c.PolyFillType>subjectFillType
+    self.clipfill    = <c.PolyFillType>clipFillType
+    self.cliptype    = <c.ClipType>    clipType
+    if strictlySimple:
+      self.thisptr[0].StrictlySimple   (True)
+    if reverseSolution:
+      self.thisptr[0].ReverseSolution  (True)
+    if preserveCollinear:
+      self.thisptr[0].PreserveCollinear(True)
+    
   def __dealloc__(self): del self.thisptr
 
-  cpdef bool AddSubjects(self, ClipperPaths paths, bool pathsAreClosed=True):
-    return self.thisptr[0].AddPaths(paths.thisptr[0], c.ptSubject, pathsAreClosed)
-  cpdef bool AddClips   (self, ClipperPaths paths):
-    return self.thisptr[0].AddPaths(paths.thisptr[0], c.ptClip,    True)
+  cpdef bool AddPaths   (self, ClipperPaths paths, c.PolyType typ, bool pathsAreClosed=True): return self.thisptr[0].AddPaths(paths.thisptr[0], typ,         pathsAreClosed)
+  def        AddSubjects(self, ClipperPaths paths,                 bool pathsAreClosed=True): return self.           AddPaths(paths,            c.ptSubject, pathsAreClosed)
+  def        AddClips   (self, ClipperPaths paths                                          ): return self.           AddPaths(paths,            c.ptClip,    True)
   
-  cpdef Clear(self):
+  def clear(self):
     self.thisptr[0].Clear()
 
-  cpdef bool ExecuteWithPaths   (self, ClipperPaths    solution=None):
-    if solution is None:
-      solution = ClipperPaths()
-    return self.thisptr[0].Execute(<c.ClipType>self.clipType, solution.thisptr[0], self.subjectfill, self.clipfill)
+  cpdef bool ExecuteWithPaths   (self, ClipperPaths    solution):
+    return self.thisptr[0].Execute(self.cliptype, solution.thisptr[0], self.subjectfill, self.clipfill)
 
-  cpdef bool ExecuteWithPolyTree(self, ClipperPolyTree solution=None):
-    if solution is None:
-      solution = ClipperPaths()
-    return self.thisptr[0].Execute(<c.ClipType>self.clipType, solution.thisptr[0], self.subjectfill, self.clipfill)
+  cpdef bool ExecuteWithPolyTree(self, ClipperPolyTree solution):
+    return self.thisptr[0].Execute(self.cliptype, solution.thisptr[0], self.subjectfill, self.clipfill)
     
-  cpdef bool Execute            (self, object          solution=None):
-    if   solution is None:                      return self.ExecuteWithPolyTree()
-    elif isinstance(solution, ClipperPaths):    return self.ExecuteWithPaths   (<ClipperPaths>   solution)
+  def        Execute            (self, object          solution):
+    if   isinstance(solution, ClipperPaths   ): return self.ExecuteWithPaths   (<ClipperPaths>   solution)
     elif isinstance(solution, ClipperPolyTree): return self.ExecuteWithPolyTree(<ClipperPolyTree>solution)
-    return False
+    else                                      : raise ValueError('Object of incorrect type: '+type(solution))
 
 
 
@@ -226,35 +302,49 @@ cdef class ClipperOffset:
     def __set__(self, double val): self.thisptr[0].ArcTolerance = val
 
   property delta:
-    def __get__(self):    return self.delta
-    def __set__(self, bool val): self.delta = val
+    def __get__(self):      return self._delta
+    def __set__(self, double val): self._delta = val
 
-  def __cinit__  (self, double delta=1.0):
-    self.thisptr = new c.ClipperOffset()
-    self.delta   = delta
+  def __cinit__  (self, double miterLimit=3.0, double arcTolerance=3.0, double delta=1.0):
+    self.thisptr                 = new c.ClipperOffset()
+    self._delta                  = delta
+    self.thisptr[0].MiterLimit   = miterLimit
+    self.thisptr[0].ArcTolerance = arcTolerance
   def __dealloc__(self): del self.thisptr
 
-  cpdef AddPaths(self, ClipperPaths paths, int joinType=c.jtRound, int endType=c.etOpenRound):
+  def AddPaths(self, ClipperPaths paths, int joinType=c.jtRound, int endType=c.etClosedPolygon):
     self.thisptr[0].AddPaths(paths.thisptr[0], <c.JoinType>joinType, <c.EndType>endType)
 
-  cpdef Clear(self):
+  def clear(self):
     self.thisptr[0].Clear()
 
-  cpdef ExecuteWithPaths   (self, ClipperPaths    solution=None):
-    if solution is None:
-      solution = ClipperPaths()
-    self.thisptr[0].Execute(solution.thisptr[0], self.delta)
+  cpdef ExecuteWithPaths   (self, ClipperPaths    solution):
+    self.thisptr[0].Execute(solution.thisptr[0], self._delta)
 
-  cpdef ExecuteWithPolyTree(self, ClipperPolyTree solution=None):
-    if solution is None:
-      solution = ClipperPolyTree()
-    self.thisptr[0].Execute(solution.thisptr[0], self.delta)
+  cpdef ExecuteWithPolyTree(self, ClipperPolyTree solution):
+    self.thisptr[0].Execute(solution.thisptr[0], self._delta)
 
-  cpdef Execute            (self, object          solution=None):
-    if   solution is None:                      return self.ExecuteWithPolyTree()
-    elif isinstance(solution, ClipperPaths):    return self.ExecuteWithPaths   (<ClipperPaths>   solution)
-    elif isinstance(solution, ClipperPolyTree): return self.ExecuteWithPolyTree(<ClipperPolyTree>solution)
-    return False
+  def   Execute            (self, object          solution):
+    if   isinstance(solution, ClipperPaths   ): self.ExecuteWithPaths   (<ClipperPaths>   solution)
+    elif isinstance(solution, ClipperPolyTree): self.ExecuteWithPolyTree(<ClipperPolyTree>solution)
+    else                                      : raise ValueError('Object of incorrect type: '+type(solution))
 
 
+#strides have to be computed just once
+cdef cnp.npy_intp *pointstrides = [sizeof(c.IntPoint),
+                                   <cnp.uint8_t*>&(<c.IntPoint*>NULL).Y - 
+                                   <cnp.uint8_t*>&(<c.IntPoint*>NULL).X]
+
+cdef cnp.ndarray Path2arrayView(ClipperPaths parent, c.Path *path):
+  """Similar to Polygon2arrayI, but instead of allocating a full-blown array,
+  the returned array is a view into the underlying data"""
+  cdef void         *data  = &(path[0][0].X)
+  cdef cnp.npy_intp *dims  = [path[0].size(),2]
+  cdef cnp.ndarray  result = cnp.PyArray_New(np.ndarray, 2, dims, cnp.NPY_INT64, pointstrides,
+                                             data, -1, NPY_ARRAY_CARRAY, <object>NULL)
+  ##result.base is of type PyObject*, so no reference counting with this assignment
+  result.base              = <ref.PyObject*>parent
+  ref.Py_INCREF(parent) #so, make sure that "result" owns a reference to "parent"
+  #ref.Py_INCREF(result)
+  return result
   
