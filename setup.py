c9fabd777bbeb1c42c37769b4acfe0df3f8ef99d
josedavid <josedavid@debianico.umons.ac.be>
2015-05-28 18:30:23 +0200

    enable compilation of Clipper.pyx in winpython 64 (the other pyx files are rather problematic)

--- setup.py
+++ setup.py
@@ -16,40 +16,85 @@
 
 # python setup.py build_ext --inplace
 
-#for this script to work properly, the Slic3r c++ libraries must have been compiled in ./deps/Slic3r/Slic3r-build
+#for this script to work properly in LINUX, the Slic3r c++ libraries must have been compiled in ./deps/Slic3r/Slic3r-build
 
-import build as b
+import os
+
+WINDOWS = os.name=='nt'
+
+if WINDOWS:
+
+  import os
+  import os.path as op
+  import shutil
+
+  from distutils.core      import setup
+  from distutils.extension import Extension
+  from distutils.sysconfig import get_config_vars
+
+  from Cython.Distutils import build_ext
+  import numpy as n
+
+  name          = "pyslic3r"
+  WINDOWS       = os.name=="nt"
+
+  SEPARATED_MODULES = not WINDOWS
+
+  description = "Python wrapper for ClipperLib library"
+
+  dir1 = "deps/Slic3r/Slic3r/xs/src"
+
+  setup(
+    name        = name+".Clipper",
+    cmdclass    = {'build_ext': build_ext},
+    description = description,
+    packages    = [name],
+    ext_modules = [
+      Extension(
+          name+".Clipper", 
+          sources=[name+"/Clipper.pyx", op.join(dir1, 'clipper.cpp')],
+          language="c++",
+          include_dirs=[dir1, n.get_include()])])
 
 
-basepath        = '.'
-
-#directory (and name) of the Cython library
-dirname         = "pyslic3r"
-
-#path of the slic3r C++ library
-libpath         = 'deps/Slic3r/cmakebuild'
-
-#path to the slic3r C++ library at runtime
-runtimelibdirs  = ["$ORIGIN"]
-
-#Cython modules
-if b.ALSOSLIC3R:
-  pynames       = ["Clipper", "SlicedModel", "TriangleMesh"]
 else:
-  pynames       = ["Clipper"]
 
-#compose the names of the libraries at link time
-libraries       = b.external_libraries(dirname, b.libnamesLD)
+  import build as b
 
-description     = "Python wrapper for Scli3r C++ library"
 
-additionalSetup = {'package_data': {dirname: b.instantiate_libnames(b.libnamesLD)} }
+  basepath        = '.'
 
-additionalExts  = {}
+  #directory (and name) of the Cython library
+  dirname         = "pyslic3r"
 
-if __name__ == "__main__":
+  #path of the slic3r C++ library
+  libpath         = 'deps/Slic3r/cmakebuild'
 
-  b.copy_external_libraries(basepath, libpath, dirname, b.libnamesLD)
-  
-  b.dobuild(b.opt, basepath, b.includepaths, dirname, description, runtimelibdirs, libraries, pynames, additionalSetup, additionalExts)
+  #path to the slic3r C++ library at runtime
+  runtimelibdirs  = ["$ORIGIN"]
+
+
+  #compose the names of the libraries at link time
+  libraries       = b.external_libraries(dirname, b.libnamesLD)
+
+  description     = "Python wrapper for Scli3r C++ library"
+
+  #Cython modules
+  if b.ALSOSLIC3R: pynames = ["Clipper", "SlicedModel", "TriangleMesh"]
+  else:            pynames = ["Clipper"]
+
+  if b.ALSOSLIC3R: additionalSetup = {'package_data': {dirname: b.instantiate_libnames(b.libnamesLD)} }
+  else:            additionalSetup = {}
+
+  if b.ALSOSLIC3R: additionalFiles = []
+  else:            additionalFiles = ['deps/Slic3r/Slic3r/xs/src/clipper.cpp']
+
+
+  additionalExts  = {}
+
+  if __name__ == "__main__":
+
+    b.copy_external_libraries(basepath, libpath, dirname, b.libnamesLD)
+    
+    b.dobuild(b.opt, basepath, b.includepaths, dirname, description, runtimelibdirs, libraries, pynames, additionalSetup, additionalExts, additionalFiles)
 
